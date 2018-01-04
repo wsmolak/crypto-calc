7dc14c0a375f5aa65cc489bff1004c51b36a4e6c
wsmolak <wsmolak@gmail.com>
2018-01-04 10:42:04 +0100

    .

--- coinmarketcap_test.py
+++ coinmarketcap_test.py
@@ -3,16 +3,20 @@ import json
 import sys
 
 if len(sys.argv) != 1:
-	config = json.load(open(sys.argv[1]))
-	#print(config.keys())
-	with urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/?limit=0") as f:
-		json = json.loads(f.read())
-		currencies = filter(lambda x: x['symbol'] in config.keys(), json)
-		sum = 0
-		for curr in currencies:
-			value = float(curr['price_usd']) * float(config[curr['symbol']])
-			print("{0} {1}: {2:.2f} USD".format(config[curr['symbol']],curr['symbol'],value))
-			sum+=value
-		print("total balance = {0:.2f} USD".format(sum))
+    config = json.load(open(sys.argv[1]))
+    proxies = config['proxy']
+    if proxies != "None":
+        opener = urllib.request.FancyURLopener(proxies)
+    else:
+        opener = urllib.request.FancyURLopener({})
+    with opener.open("https://api.coinmarketcap.com/v1/ticker/?limit=0") as f:
+        json = json.loads(f.read())
+        currencies = filter(lambda x: x['symbol'] in config.keys(), json)
+        sum = 0
+        for curr in currencies:
+            value = float(curr['price_usd']) * float(config[curr['symbol']])
+            print("{0} {1}: {2:.2f} USD".format(config[curr['symbol']], curr['symbol'], value))
+            sum += value
+        print("total balance = {0:.2f} USD".format(sum))
 else:
-	print("usage: python " + sys.argv[0] + " config.json")
+    print("usage: python " + sys.argv[0] + " config.json")
