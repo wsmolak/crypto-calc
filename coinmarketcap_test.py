f2e77621316ff5726be633b3551fe3eba31f7668
wsmolak <wsmolak@gmail.com>
2018-01-16 13:49:51 +0100

    storing data in mongodb

--- coinmarketcap_test.py
+++ coinmarketcap_test.py
@@ -5,31 +5,49 @@ import json
 import sys
 import time
 from functools import reduce
+from pymongo import MongoClient
+
+
+def store_data(data, db_client):
+    db = db_client.coin_calc_db
+    calc_values = db.calc_values
+    inserted_id = calc_values.insert_one(data)
+    return inserted_id
+
+
+def connect_to_mongo():
+    db_config = json.load(open("db_config.json"))
+    client = MongoClient(db_config['db_host'], db_config['db_port'])
+    return client
+
 
 if len(sys.argv) != 1:
-	config = json.load(open(sys.argv[1]))
-	with urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/?limit=0") as f:
-		res = f.read()
-		json_res = json.loads(res.decode())
-	currencies = filter(lambda x: x['symbol'] in config.keys(), json_res)
-	sum = 0
-	timestamp = time.time()
-	output = {'time': timestamp}
-	output['balances'] = dict()
-	output['rates'] = dict()
-	for curr in currencies:
-		value = float(curr['price_usd']) * float(config[curr['symbol']])
-		#print("{0} {1}: {2:.2f} USD".format(config[curr['symbol']],curr['symbol'],value))
-		#sum+=value
-		output['balances'][curr['symbol']]= value
-		output['rates'][curr['symbol']] = float(curr['price_usd'])
-	#print("total balance = {0:.2f} USD".format(sum))
-	json_string = json.dumps(output,indent=4)
-	print(json_string)
-	json_out = json.loads(json_string)
-	#print( json_out['balances'].values())
-	sum = reduce(lambda x,y: x+y, json_out['balances'].values())
-	print(sum)
+    config = json.load(open(sys.argv[1]))
+    with urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/?limit=0") as f:
+        res = f.read()
+        json_res = json.loads(res.decode())
+    currencies = filter(lambda x: x['symbol'] in config.keys(), json_res)
+    timestamp = time.time()
+    output = dict()
+    output['time'] = timestamp
+    output['balances'] = dict()
+    output['rates'] = dict()
+
+    for curr in currencies:
+        value = float(curr['price_usd']) * float(config[curr['symbol']])
+        output['balances'][curr['symbol']] = value
+        output['rates'][curr['symbol']] = float(curr['price_usd'])
+
+    output['sums'] = reduce(lambda x, y: x + y, output['balances'].values())
+    json_string = json.dumps(output)
+
+    inserted = store_data(json_string, connect_to_mongo())
+    if inserted is not None:
+        print(inserted)
+        sys.exit()
+
 else:
-	print("usage: python " + sys.argv[0] + " config.json")
-	
+    print("usage: python " + sys.argv[0] + " config.json")
+
+
+
